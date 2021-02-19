3f52fe854b90b00452fe7f22749c9d0eeed73f90
sravanin15 <sravaninuthanapati15@gmail.com>
2021-02-19 12:12:07 +0530

    first commit

---
+++ PipelineSonarqube/src/main/java/com/mindtree/PipelineSonarqube/App.java
@@ -0,0 +1,14 @@
+package com.mindtree.PipelineSonarqube;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("sonar qube integration");
+    }
+}
